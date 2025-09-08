cdf8e40ae0dcf803e106631dcf237d4b21820212
Aparecido-Junior <aparecido.jfilho@outlook.com>
2025-09-08 21:37:00 +1000

    assignments2 update

--- assignments_section2.sql
+++ assignments_section2.sql
@@ -1,5 +1,5 @@
 -- Connect to database
-
+USE maven_advanced_sql;
 
 -- ASSIGNMENT 1: Basic Joins
 -- Looking at the orders and products tables, which products exist in one table, but not the other?
@@ -20,11 +20,41 @@
 -- Which products are within 25 cents of each other in terms of unit price?
 
 -- View the products table
-
+SELECT * FROM products;
 
 -- Join the products table with itself so each candy is paired with a different candy
-
+SELECT		p1.product_name, p1.unit_price, 
+			p2.product_name, p2.unit_price,
+            p1.unit_price - p2.unit_price AS price_diff
+FROM products p1
+INNER JOIN products p2 ON p1.product_id < p2.product_id;
         
 -- Calculate the price difference, do a self join, and then return only price differences under 25 cents
-
-
+SELECT		p1.product_name, p1.unit_price, 
+			p2.product_name, p2.unit_price,
+            p1.unit_price - p2.unit_price AS price_diff
+FROM products p1
+INNER JOIN products p2 ON p1.product_id < p2.product_id
+WHERE		p1.unit_price <> p2.unit_price AND
+			p1.unit_price - p2.unit_price BETWEEN -0.25 AND  0.25
+ORDER BY p1.unit_price DESC;
+/*
++-----------------------------------+------------+-----------------------------------+------------+------------+
+| product_name                      | unit_price | product_name                      | unit_price | price_diff |
++-----------------------------------+------------+-----------------------------------+------------+------------+
+| Wonka Bar - Fudge Mallows         |       3.60 | Wonka Bar - Nutty Crunch Surprise |       3.49 |       0.11 |
+| Wonka Bar - Fudge Mallows         |       3.60 | Wonka Bar - Triple Dazzle Caramel |       3.75 |      -0.15 |
+| Wonka Bar - Fudge Mallows         |       3.60 | Fizzy Lifting Drinks              |       3.75 |      -0.15 |
+| Wonka Bar - Scrumdiddlyumptious   |       3.60 | Wonka Bar - Triple Dazzle Caramel |       3.75 |      -0.15 |
+| Wonka Bar - Scrumdiddlyumptious   |       3.60 | Fizzy Lifting Drinks              |       3.75 |      -0.15 |
+| Wonka Bar - Nutty Crunch Surprise |       3.49 | Wonka Bar - Scrumdiddlyumptious   |       3.60 |      -0.11 |
+| Wonka Bar - Nutty Crunch Surprise |       3.49 | Kazookles                         |       3.25 |       0.24 |
+| Wonka Bar - Milk Chocolate        |       3.25 | Wonka Bar - Nutty Crunch Surprise |       3.49 |      -0.24 |
+| Fun Dip                           |       1.50 | Pixy Stix                         |       1.25 |       0.25 |
+| Nerds                             |       1.50 | Pixy Stix                         |       1.25 |       0.25 |
+| Wonka Gum                         |       1.25 | Fun Dip                           |       1.50 |      -0.25 |
+| Wonka Gum                         |       1.25 | Nerds                             |       1.50 |      -0.25 |
+| Wonka Gum                         |       1.25 | SweeTARTS                         |       1.50 |      -0.25 |
+| Pixy Stix                         |       1.25 | SweeTARTS                         |       1.50 |      -0.25 |
++-----------------------------------+------------+-----------------------------------+------------+------------+
+*/
